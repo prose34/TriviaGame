2c55d849673ba292503bbe14d8a72eccad2ceedf
Paul Rose <prose2334@gmail.com>
2018-03-26 12:02:20 -0500

    add html

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// What is Spider-Man's real name?
+// Who is not a member of the original Fantastic Four?
+// who is the strongest character?
+// Daredevil has which physical disability?
+// Cyclops is a member of which team?
+
+
+// Press the start button - on click function and display questions
+// start 30 second interval timer
+// provide only one answer per radio button
+// if click submit end the timer and run function to check answers
+// if timer runs out say game over time out 0 score?
+
+// list correct, incorrect, unaswered questions
+
+
