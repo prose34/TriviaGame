2aab873ef5e99548f5b273fba73a077f10e31e39
Paul Rose <prose2334@gmail.com>
2018-03-26 14:57:37 -0500

    more changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,3 @@
-// What is Spider-Man's real name?
-// Who is not a member of the original Fantastic Four?
-// who is the strongest character?
-// Daredevil has which physical disability?
-// Cyclops is a member of which team?
-
 
 // Press the start button - on click function and display questions
 // start 30 second interval timer
@@ -14,3 +8,112 @@
 // list correct, incorrect, unaswered questions
 
 
+// when the html has loaded, run the following code
+$(document).ready(function() {
+
+
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var unaswered = 0;
+    var timer = 30;
+
+    // hide the questions and end results until they are called
+    $(".gameQuestions").hide();
+    $(".endGame").hide();
+
+    //  hide the starter container when it is clicked and pull up the question container
+    $(".starter").on("click", function () {
+        $(".starter").hide();
+        $(".gameQuestions").show();
+        countdownInterval(); //start the timer when the questions are displayed
+    })
+
+    // create a function that will decrement the timer by one and end the game if the timer is up or
+    // if the user clicks submit answers
+    function countdown () {
+        timer--;
+        
+        $('#timeRemaining').html(timer + " Seconds");
+
+        if (timer < 0) {
+            $(".gameQuestions").hide();
+            $(".endGame").show();
+            clearInterval(countdown);
+            checkResults(); // move on to check results
+        }
+
+        $(".submitButton").on("click", function() {
+            $(".gameQuestions").hide();
+            $(".endGame").show();
+            clearInterval(countdown);
+            checkResults(); //move on to check results
+
+        })
+    }
+
+    // this function gives the countdown function an increment of 1 second
+    function countdownInterval () {
+        setInterval(countdown, 1000);
+
+    }
+
+    function checkResults () {
+
+
+        var Q1 = $('input:radio[name="q1"]:checked').val();
+        var Q2 = $('input:radio[name="q2"]:checked').val();
+        var Q3 = $('input:radio[name="q3"]:checked').val();
+        var Q4 = $('input:radio[name="q4"]:checked').val();
+        var Q5 = $('input:radio[name="q5"]:checked').val();
+    
+        if (Q1 === "Peter Parker") {
+            correctAnswers++;
+        } else if (Q1 === undefined) {
+            unaswered++;
+        } else {
+            incorrectAnswers++;
+        }
+       
+        if (Q2 === "Black Widow") {
+            correctAnswers++;
+        } else if (Q2 === undefined) {
+            unaswered++;
+        } else {
+            incorrectAnswers++;
+        }
+
+        if (Q3 === "Hulk") {
+            correctAnswers++;
+        } else if (Q3 === undefined) {
+            unaswered++;
+        } else {
+            incorrectAnswers++;
+        }
+
+        if (Q4 === "Blind") {
+            correctAnswers++;
+        } else if (Q4 === undefined) {
+            unaswered++;
+        } else {
+            incorrectAnswers++;
+        }
+
+        if (Q5 === "X-men") {
+            correctAnswers++;
+        } else if (Q5 === undefined) {
+            unaswered++;
+        } else {
+            incorrectAnswers++;
+        }
+
+        $("#correctAnswers").html(correctAnswers);
+        $("#incorrectAnswers").html(incorrectAnswers);
+        $("#unanswered").html(unaswered);
+
+
+
+    }
+
+
+
+});
