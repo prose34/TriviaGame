e058ad81017efedf712c289558d83a231378ad41
Paul Rose <prose2334@gmail.com>
2018-03-26 18:24:58 -0500

    more changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,8 @@ $(document).ready(function() {
     var correctAnswers = 0;
     var incorrectAnswers = 0;
     var unaswered = 0;
-    var timer = 3;
+    var timer = 5;
+    var timeDecrement;
 
     // hide the questions and end results until they are called
     $(".gameQuestions").hide();
@@ -29,34 +30,37 @@ $(document).ready(function() {
     // create a function that will decrement the timer by one and end the game if the timer is up or
     // if the user clicks submit answers button
     function countdown () {
+
         timer--;
+
         $('#timeRemaining').html(timer + " Seconds");
-    }
-
-    // this function makes the countdown function repeat at an increment of 1 second
-    function countdownInterval () {
-
-        var timeDecrement = setInterval(countdown, 1000);
 
         if (timer < 0) {
             $(".gameQuestions").hide();
             $(".endGame").show();
+            // clearInterval(timeDecrement);
             checkResults(); // move on to check results
-            clearInterval(timeDecrement);
 
-        }
-
-        $(".submitButton").on("click", function() {
-            $(".gameQuestions").hide();
-            $(".endGame").show();
-            checkResults(); //move on to check results
-            clearInterval(timeDecrement);
-
-        })
+        };
 
     };
 
+    // this function makes the countdown function repeat at an increment of 1 second
+    function countdownInterval () {
+
+       timeDecrement = setInterval(countdown, 1000);
+
+    };
+
+    $(".submitButton").on("click", function() {
+            $(".gameQuestions").hide();
+            $(".endGame").show();
+            checkResults(); //move on to check results
+    });
+
     function checkResults () {
+
+        clearInterval(timeDecrement);
 
         var Q1 = $('input:radio[name="q1"]:checked').val();
         var Q2 = $('input:radio[name="q2"]:checked').val();
@@ -64,37 +68,37 @@ $(document).ready(function() {
         var Q4 = $('input:radio[name="q4"]:checked').val();
         var Q5 = $('input:radio[name="q5"]:checked').val();
     
-        if (Q1 === "Peter Parker") {
+        if (Q1 == "Peter Parker") {
             correctAnswers++;
-        } else if (Q1 === undefined) {
+        } else if (Q1 == undefined) {
             unaswered++;
         } else {
             incorrectAnswers++;
-        }
+        };
        
-        if (Q2 === "Black Widow") {
+        if (Q2 == "Black Widow") {
             correctAnswers++;
-        } else if (Q2 === undefined) {
+        } else if (Q2 == undefined) {
             unaswered++;
         } else {
             incorrectAnswers++;
-        }
+        };
 
-        if (Q3 === "Hulk") {
+        if (Q3 == "Hulk") {
             correctAnswers++;
-        } else if (Q3 === undefined) {
+        } else if (Q3 == undefined) {
             unaswered++;
         } else {
             incorrectAnswers++;
-        }
+        };
 
-        if (Q4 === "Blind") {
+        if (Q4 == "Blind") {
             correctAnswers++;
-        } else if (Q4 === undefined) {
+        } else if (Q4 == undefined) {
             unaswered++;
         } else {
             incorrectAnswers++;
-        }
+        };
 
         if (Q5 === "X-men") {
             correctAnswers++;
@@ -102,7 +106,7 @@ $(document).ready(function() {
             unaswered++;
         } else {
             incorrectAnswers++;
-        }
+        };
 
         $("#correctAnswers").html(correctAnswers);
         $("#incorrectAnswers").html(incorrectAnswers);
@@ -122,7 +126,7 @@ $(document).ready(function() {
         correctAnswers = 0;
         incorrectAnswers = 0;
         unaswered = 0;
-        timer = 30;
+        timer = 5;
     };
 
 });
